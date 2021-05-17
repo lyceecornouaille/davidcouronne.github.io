eabdc0078f7e3e9817b33f59a49d1a1754061909
David Couronné <couronne.david@gmail.com>
2021-05-17 14:39:44 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,13 +89,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/DavidCouronne/davidcouronne.github.io/edit/master/website/',
+            'https://github.com/DavidCouronne/davidcouronne.github.io/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/DavidCouronne/davidcouronne.github.io/edit/master/website/blog/',
+            'https://github.com/DavidCouronne/davidcouronne.github.io/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
